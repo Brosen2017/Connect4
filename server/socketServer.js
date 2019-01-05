677a494d9167ddf44cefc6b378357b6099206345
Brosen2017 <brandonrosenstein@gmail.com>
2019-01-05 12:29:48 -0800

    cleaned up code and added comments to socket server

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,73 +1,40 @@
 const app = require('express')();
 const http = require('http').Server(app)
 var io = require('socket.io')(http);
-const db = require('../database/index.js')
+//const db = require('../database/index.js')
+const PORT = 1337;
 let store = [];
 let bucket = [];
-//let rooms = [1,2,3,4,5,6,7,8,9,10]
 let rooms = [1]
 
-// for(var i = 1; i < 100; i++ ){
-//     rooms.push(i)
-// }
 
-
-
-const PORT = 1337;
 
 
 var roomno = 1;
 io.on('connection', (socket)=>{
     
-     if(rooms[roomno-1] !== roomno){
+    if(rooms[roomno-1] !== roomno){
          rooms.push(roomno)
     }
     
-    
-    console.log('current rooms', rooms)
     if(store.length < 2){
     store.push(socket.id)
     }
 
-
+    //create a new room every time two players join into a room
     if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
    socket.join("room-"+roomno);
 
-
+   //send the user room to the connected clients, push the toom into the room bucket and reset store array 
    io.sockets.in("room-"+roomno).emit('connectToRoom', {player: store, room: +roomno});
    if(store.length === 2){
     bucket.push(store)
     store = []
     }
-    
-  
 
-    // socket.broadcast.emit('user connected');
-    //console.log('user connected', socket.id, bucket);
-    //all listeners and emitters will be inside of the connection
-
-    // socket.on('join',(player)=>{
-    //     db.Player.find({
-    //         Username: player 
-    // }, (err, data)=>{
-    //     if(err){
-    //         throw(err)
-    //     } else {
-    //         // console.log('db data', data)
-    //         if(data.length <= 0){
-    //             io.emit('join', "taken")
-    //         } else {
-    //             io.emit('join', player)
-    //         }
-    //     }
-    // })
-    // })
-
-    //now that lobby has been refactored to load dynamically based on room, must refactor other 
-    //socket code to attribute on a per room basis
-
+    //verify user rooms are full on a per room basis, if true game will start, if not loading screen will open
     socket.on('lobby', (room, player)=>{
-        console.log('client room!', player)
+        //console.log('client room!', player)
         for(var i=0; i < bucket.length; i++){
             if(JSON.stringify(bucket[i]) === JSON.stringify(player)){
                 if(bucket[room -1].length >= 2){
@@ -80,33 +47,31 @@ io.on('connection', (socket)=>{
         }
     })
 
-
+    //verify the current room and room number, if they match check the current player and emit the next turn the other player connected in the room
     socket.on('toggle',(currentPlayer, array, room)=>{
-        //check if user at store[0] and he will return toggle 2
-        //check if user at store[1] and he will return toggle 1
-        //console.log('current player in toggle', currentPlayer, room, array)
         for(var i=0; i < bucket.length; i++){
             if(JSON.stringify(bucket[i]) === JSON.stringify(array) && room === rooms[room-1]){
                 if(currentPlayer === bucket[i][0]){
-                    console.log('player 1', currentPlayer)
+                    //console.log('player 1', currentPlayer)
                 socket.broadcast.in("room-"+rooms[room-1]).emit('toggle', 2)
                 }
                 if(currentPlayer === bucket[i][1]){
-                    console.log('player 2', currentPlayer)
+                    //console.log('player 2', currentPlayer)
                     socket.broadcast.in("room-"+rooms[room-1]).emit('toggle', 1)
                 }
     }
 }
     })
 
-
+    //refresh the board based on the room its coming from
     socket.on('board', (board, room)=>{
         io.in("room-"+room).emit('board', board)
     })
 
+    //this function is only called when a player wins or draws, and it emits the winning player to all connected in the room
     socket.on('player', (player, room)=>{
-        
-        console.log('here is the current player:', player)
+
+        //console.log('here is the current player:', player)
         if(player === 1){
             io.in("room-"+room).emit('player', 'Red wins!')   
         }
@@ -119,8 +84,9 @@ io.on('connection', (socket)=>{
     
     })
 
+    //verify the room the user disconnected from, and let the other player in the room know that they can start a new match
     socket.on('disconnect', function(){
-        console.log('user disconnected', socket.id);
+        //console.log('user disconnected', socket.id);
         for(var i = 0; i < bucket.length; i++){
             for(x =0; x < bucket[i].length; x++){
                 if(bucket[i][x] === socket.id){
@@ -131,9 +97,6 @@ io.on('connection', (socket)=>{
                 }
             }
         }
-        //incorporate logic to implemement win or loss if someone disconnects
-        //if user disconnects return other player to loading screen
-        //if another user connects, push them into the room with an open space
       });
 })
 
