06ab944cbdcde8d41bb1f512e4587a86f8b68a2e
Brosen2017 <brandonrosenstein@gmail.com>
2019-01-04 16:59:13 -0800

    minor tweaks

--- server/socketServer.js
+++ server/socketServer.js
@@ -2,26 +2,19 @@ const app = require('express')();
 const http = require('http').Server(app)
 var io = require('socket.io')(http);
 const db = require('../database/index.js')
-let currPlayer = null;
 let store = [];
 let bucket = [];
 let rooms = [1,2,3,4,5,6,7,8,9,10]
 
 const PORT = 1337;
 
-//all the server side listeners and emitters go here
- var roomno = 1;
-io.on('connection', (socket)=>{
-    // for(var i = 0; i < store.length; i++){
 
-    // }
+var roomno = 1;
+io.on('connection', (socket)=>{
     if(store.length < 2){
     store.push(socket.id)
     }
-    // if(store.length === 2){
-    // bucket.push(store)
-    // store = []
-    // }
+
 
     if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
    socket.join("room-"+roomno);
@@ -33,11 +26,10 @@ io.on('connection', (socket)=>{
     store = []
     }
     
-    //store.push(socket.id)
   
 
     // socket.broadcast.emit('user connected');
-    console.log('user connected', socket.id, bucket);
+    //console.log('user connected', socket.id, bucket);
     //all listeners and emitters will be inside of the connection
 
     // socket.on('join',(player)=>{
@@ -60,32 +52,20 @@ io.on('connection', (socket)=>{
     //now that lobby has been refactored to load dynamically based on room, must refactor other 
     //socket code to attribute on a per room basis
 
-        socket.on('lobby', (room, player)=>{
+    socket.on('lobby', (room, player)=>{
         console.log('client room!', player)
         for(var i=0; i < bucket.length; i++){
             if(JSON.stringify(bucket[i]) === JSON.stringify(player)){
                 if(bucket[room -1].length >= 2){
-            //if(store.length >= 2){
-            // if(roomno === 2){
-            // io.in("room-"+roomno).emit('lobby', false)
-            // } else {
-            //io.emit('lobby', true)
-            //}else{
-            //io.emit('lobby', false)
-            //}
-            io.in("room-"+room).emit('lobby', true)
+                    io.in("room-"+room).emit('lobby', true)
 
-        } else {
-            io.in("room-"+room).emit('lobby', false)
+                } else {
+                io.in("room-"+room).emit('lobby', false)
+                }
+            }
         }
-    }
-    }
     })
 
-    // socket.on('update',(currentPlayer)=>{
-    //     currPlayer = currentPlayer
-    //     console.log('player in update', currPlayer)
-    // })
 
     socket.on('toggle',(currentPlayer, array, room)=>{
         //check if user at store[0] and he will return toggle 2
@@ -93,13 +73,11 @@ io.on('connection', (socket)=>{
         //console.log('current player in toggle', currentPlayer, room, array)
         for(var i=0; i < bucket.length; i++){
             if(JSON.stringify(bucket[i]) === JSON.stringify(array) && room === rooms[room-1]){
-        //implement logic to compare bucket[0][0]
                 if(currentPlayer === bucket[i][0]){
                 //if(currentPlayer === store[0]){
                     console.log('player 1', currentPlayer)
                 socket.broadcast.in("room-"+rooms[room-1]).emit('toggle', 2)
                 }
-                //implement logic to compare bucket[0][1]
                 if(currentPlayer === bucket[i][1]){
                 //if(currentPlayer === store[1]){
                     console.log('player 2', currentPlayer)
@@ -109,13 +87,8 @@ io.on('connection', (socket)=>{
 }
     })
 
-    // socket.on('place',(piece)=>{
-    //     //console.log('piece placed at index:', piece)
-    //     io.emit('place', piece)
-    // })
 
     socket.on('board', (board, room)=>{
-        // console.log('here is the new board:', board)
         io.in("room-"+room).emit('board', board)
     })
 
@@ -135,8 +108,10 @@ io.on('connection', (socket)=>{
     })
 
     socket.on('disconnect', function(){
-        //store.pop();
         console.log('user disconnected');
+        //incorporate logic to implemement win or loss if someone disconnects
+        //if useor disconnects return other player to loading screen
+        //if another user connects, push them into the room with an open space
       });
 })
 
