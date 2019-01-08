435f3f2d72911fbd29143c6c50e750ead1be6964
brandon <brandonrosenstein@gmail.com>
2019-01-07 17:39:06 -0800

    cleaned up code and added comments for easier readability

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,77 +1,83 @@
-import io from 'socket.io-client';
-const  socket = io('http://localhost:1337');
+import io from "socket.io-client";
+const socket = io("http://localhost:1337");
 
-//all of the listeners and emitters for client go here
+//all of the listeners and emitters for client happen here
 
 function joinGame(cb) {
-    // socket.emit('join', player);
-    socket.on('connectToRoom',(data)=> {
-      cb(data)
-    })
-  }
-
-function toggleData(cb){
-  socket.on('connectToRoom',(data)=> {
-    cb(data)
-  })
+  socket.on("connectToRoom", data => {
+    cb(data);
+  });
 }
 
-function updateName(cb){
-  socket.on('join', (p)=>{
+function toggleData(cb) {
+  socket.on("connectToRoom", data => {
+    cb(data);
+  });
+}
+
+function updateName(cb) {
+  socket.on("join", p => {
     cb(p);
-  }); 
+  });
 }
 
-  function updateBoard(board, room){
-    socket.emit('board', board, room);
-  }
+function updateBoard(board, room) {
+  socket.emit("board", board, room);
+}
 
-  function retrieveBoard(cb){
+function retrieveBoard(cb) {
+  socket.on("board", b => {
+    cb(b);
+  });
+}
 
-    socket.on('board', (b)=>{
-      cb(b);
-    })
-  }
+function toggle(array, room) {
+  socket.emit("toggle", socket.id, array, room);
+}
 
-  // function playerTrack(){
-  //   socket.emit('update', socket.id)
-  // }
+function player(cb) {
+  socket.on("toggle", currentPlayer => {
+    cb(currentPlayer);
+  });
+}
 
-  function toggle(array, room){
-    // console.log('no socket?', socket.id)
-    socket.emit('toggle', socket.id, array, room);
-  }
+function updatePlayer(player, room) {
+  socket.emit("player", player, room);
+}
 
-  function player(cb){
-    socket.on('toggle', (currentPlayer)=>{
-      cb(currentPlayer)
-    })
-  }
+function retrievePlayer(cb) {
+  socket.on("player", p => {
+    cb(p);
+  });
+}
 
-  function updatePlayer(player, room){
-    socket.emit('player', player, room)
-  }
+function lobby(room, player) {
+  socket.emit("lobby", room, player);
+}
 
-  function retrievePlayer(cb){
-    socket.on('player', (p)=>{
-      cb(p)
-    })
-  }
+function lobbyCheck(cb) {
+  socket.on("lobby", bool => {
+    cb(bool);
+  });
+}
 
-  function lobby(room, player){
-    socket.emit('lobby', room, player)
-  }
+function disconnect(cb) {
+  socket.on("disconnect", bool => {
+    cb(bool);
+  });
+}
 
-  function lobbyCheck(cb){
-    socket.on('lobby', (bool)=>{
-      cb(bool)
-    })
-  }
-
-  function disconnect(cb){
-    socket.on('disconnect',(bool)=>{
-      cb(bool)
-    })
-  }
-
-export { joinGame, disconnect, toggleData, lobby, lobbyCheck, toggle, player, updateName, updateBoard, retrieveBoard, updatePlayer, retrievePlayer};
+export {
+  joinGame,
+  disconnect,
+  toggleData,
+  lobby,
+  lobbyCheck,
+  toggle,
+  player,
+  updateName,
+  updateBoard,
+  retrieveBoard,
+  updatePlayer,
+  retrievePlayer
+};
